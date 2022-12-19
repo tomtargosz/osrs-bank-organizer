9c0eb473037a29d0f6cc52562b599f560e0ffb70
DESKTOP-J15OV5V\Tom <tom.targosz@yahoo.com>
2022-12-18 20:52:05 -0600

    log item names and ids on bank open

--- src/main/java/com/example/ExamplePlugin.java
+++ src/main/java/com/example/ExamplePlugin.java
@@ -3,14 +3,15 @@ package com.example;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.*;
+import net.runelite.api.events.ScriptPostFired;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+
 
 @Slf4j
 @PluginDescriptor(
@@ -37,11 +38,35 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	public void onScriptPostFired(ScriptPostFired event)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+		// User has opened their bank
+		if (event.getScriptId() == ScriptID.BANKMAIN_BUILD)
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			getBankItemIDs(WidgetInfo.BANK_ITEM_CONTAINER, InventoryID.BANK);
+		}
+	}
+
+	private void getBankItemIDs(WidgetInfo widgetInfo, InventoryID inventoryID)
+	{
+		final Widget widget = client.getWidget(widgetInfo);
+		final ItemContainer itemContainer = client.getItemContainer(inventoryID);
+		final Widget[] children = widget.getChildren();
+		final int maxNumberOfItemsToLog = 20;
+
+		if (itemContainer != null && children != null)
+		{
+			// In the bank, the first components are the bank items, followed by tabs etc. There are always 816 components regardless
+			// of bank size, but we only need to check up to the bank size.
+			for (int i = 0; i < itemContainer.size(); ++i)
+			{
+				Widget child = children[i];
+				if (child != null && !child.isSelfHidden() && child.getItemId() > -1 && i < maxNumberOfItemsToLog)
+				{
+					log.info("name: " + child.getName());
+					log.info("id: " + child.getItemId());
+				}
+			}
 		}
 	}
 
